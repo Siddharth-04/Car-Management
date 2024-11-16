d3008eef8ae4d75f7b9e532933b80db838825525
Siddharth <siddharth1360.be21@chitkara.edu.in>
2024-11-16 11:33:47 +0530

    Fixed Server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,12 +1,24 @@
 require('dotenv').config(); // Make sure this is at the very top
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const path = require('path'); 
-const authRoutes = require('./routes/authRoutes'); // Import auth routes
-const carRoutes = require('./routes/carRoutes'); // Import car routes
-const connectDB = require('./db');
+const authRoutes = require('./routes/authRoutes'); 
+const carRoutes = require('./routes/carRoutes');
+//const connectDB = require('./db');
 const app = express();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error);
+  }
+};
 // Connect to MongoDB
 connectDB();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));

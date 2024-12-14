545a29161b2f945b6f336a806096bef69008bf59
shubham4672 <shubham4672.20@gmail.com>
2024-12-14 21:19:08 +0530

    responsiveness fix

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../logo.svg";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -13,16 +14,16 @@ const Navbar = () => {
   const isLoggedIn = !!localStorage.getItem("token");
 
   return (
-    <nav className='navbar bg-slate-900 flex items-center lg:justify-around'>
+    <nav className='navbar bg-slate-900 flex items-center'>
       <div className='logo flex items-center gap-10'>
         <img src={logo} alt='Logo' className='w-10' />
         <h2 className='logo text-white cursor-pointer'>
-          <Link to='/home'>
+          <Link to='/home' className=' flex items-baseline'>
             Cars<small>Dex</small>
           </Link>
         </h2>
       </div>
-      <ul className='nav-links flex text-white'>
+      <ul className='nav-links flex text-white shrink'>
         {isLoggedIn ? (
           <>
             <li className='px-8 py-1 hover:bg-slate-800 rounded-full'>
